ebe7cd1e874a9517ec7b7593111b014ea6eb4b06
Daniel <dev@dwennemar.de>
2020-04-13 12:16:20 +0200

    basic implementation

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {interval} from 'rxjs';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent implements OnInit {
+  skillSet = ['Angular-Development', 'Java-Development', 'UI-Design'];
+  txt = this.skillSet[0];
+  skillIndex = 0;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.switchTxt();
+  }
+
+  switchTxt() {
+    interval(1600).subscribe((val) => {
+      if(this.skillIndex >= this.skillSet.length) {
+        this.skillIndex = 0;
+      }
+      this.txt =this.skillSet[this.skillIndex];
+      this.skillIndex++;
+    });
+  }
+}
